8d71f359bd31453b08678be31f4918598c893e68
The Ghostavo <57949820+GustavoBiscaro@users.noreply.github.com>
2024-07-23 00:03:49 -0300

    backend

--- financialControl/server/CONTROLLERS/UserController.js
+++ financialControl/server/CONTROLLERS/UserController.js
@@ -1,7 +1,10 @@
 const User = require('../MODELS/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-const createUserToken = require('../HELPERS/create-user-token.js') //
+// HELPERS
+const createUserToken = require('../HELPERS/create-user-token') 
+const getToken = require('../HELPERS/get-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -70,5 +73,68 @@ module.exports = class UserController {
         }catch(error) {
             res.status(500).json({ message: error})
         }
+    }
+
+    static async login(req, res) {
+        const {email, password} = req.body
+
+        if (!email) {
+            res.status(422).json({ message: 'Please, the email is required' })
+            return
+        }
+
+        if (!password) {
+            res.status(422).json({ message: 'Please, the password is required' })
+            return
+        }
+
+        // check if the user does not already exist
+        const user = await User.findOne({ 
+            where: {
+            email: email
+            }
+        });
+
+        if(!user) {
+            res
+                .status(422)
+                .json({
+                message: 'User with this email is not registered',
+            })
+            return
+        }
+
+        // check if password match with bcrypt
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if(!checkPassword) {
+            res
+            .status(422)
+            .json({
+            message: 'The passwords does not match!',
+        })
+        return
+        }
+
+        await createUserToken(user, req, res)
+    }
+
+    static async checkUser(req, res) {
+
+        let currentUser
+
+        if(req.headers.authorization) {
+
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'oursecret')
+
+            currentUser = await User.findByPk(decoded.id)
+
+            currentUser.password = undefined
+        } else {
+            currentUser = null
+        }
+
+        res.status(200).send(currentUser)
     }
 };
