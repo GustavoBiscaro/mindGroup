f407ecc0d1aec62753714d509dc8989f1865f405
The Ghostavo <57949820+GustavoBiscaro@users.noreply.github.com>
2024-07-28 23:05:57 -0300

    update info profile users

--- financialControl/client/vite-project/src/COMPONENTS/pages/User/Profile.jsx
+++ financialControl/client/vite-project/src/COMPONENTS/pages/User/Profile.jsx
@@ -1,3 +1,4 @@
+import api from '../../../UTILS/api';
 import React, { useState, useEffect } from 'react'
 
 import styles from './Profile.module.css'
@@ -5,28 +6,75 @@ import formStyles from '../../form/Form.module.css';
 
 import Input from './../../form/Input';
 
+import useFlashMessage from './../../../HOOKS/useFlashMessage';
+
 const Profile = () => {
 
     const [user, setUser] = useState({})
+    const [token] = useState(localStorage.getItem('token') || '')
+    const { setFlashMessage } = useFlashMessage()
+
+    useEffect(() => {
+        api.get('/users/checkuser', {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`
+            }
+        }).then((response) => {
+            setUser(response.data)
+        })
+
+
+    }, [token])
 
     function onFileChange(e) {
-
+        setUser({ ...user, [e.target.name]: e.target.files[0] })
     }
 
     function handleChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        let msgType = 'success'
+
+        const formData = new FormData()
+
+        await Object.keys(user).forEach((key) =>
+            formData.append(key, user[key])
+        )
+
+        const data = await api.patch(`/users/edit/$user._id`, formData, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+                'Content-Type': 'multipart/form-data'
+            },
+        })
+            .then((response) => {
+                return response.data
+            })
+            .catch((err) => {
+                msgType = 'error'
+                return err.response.data
+            })
+
+        setFlashMessage(data.message, msgType)
 
     }
+
+
 
     return (
 
 
         <section>
-            <div  className={styles.profile_header}>
+            <div className={styles.profile_header}>
                 <h1>Profile</h1>
                 <p>Preview Image</p>
             </div>
 
-            <form className={formStyles.form_container}>
+            <form onSubmit={handleSubmit} className={formStyles.form_container}>
                 <Input
                     text="Image"
                     type="file"
